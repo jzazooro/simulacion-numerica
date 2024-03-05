b8fde4558a708db6bad0204e9f2694a3a8d4e8cf
jzazooro <91785177+jzazooro@users.noreply.github.com>
2024-03-05 17:28:42 +0100

    ejercicio clase

---
+++ 2.ecuacion-de-onda/ejercicio1e.py
@@ -0,0 +1,45 @@
+#f(x)=x(b-x) , g(x)=0 , b=5, d=10, n=40, m=400, v=0.5 ,d es el tiempo,  b la longitud de la cuerda , m es el paso en la variable temporal, n es el paso en la variable espacial
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# Parámetros dados
+b = 5
+d = 10
+n = 40
+m = 400
+v = 5
+
+# Calculando Δx, Δt y r
+Δx = b / n
+Δt = d / m
+r = v * Δt / Δx
+
+# Inicializando la matriz u para almacenar las posiciones de la cuerda
+u = np.zeros((n+1, m+1))
+
+# Condiciones iniciales
+x = np.linspace(0, b, n+1)
+u[:, 0] = x * (b - x)  # f(x) = x(b-x)
+u[:, 1] = u[:, 0]  # Aproximando que la primera derivada temporal es 0
+
+# Aplicando el método de diferencias finitas
+for j in range(1, m):
+    for i in range(1, n):
+        u[i, j+1] = (2 - 2*r**2)*u[i, j] + r**2*(u[i+1, j] + u[i-1, j]) - u[i, j-1]
+
+# Graficando la cuerda en 3D
+X, Y = np.meshgrid(x, np.linspace(0, d, m+1))
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+# Graficando la superficie
+ax.plot_surface(X, Y, u.T, cmap='viridis')
+
+ax.set_title('Evolución de la cuerda en el tiempo en 3D')
+ax.set_xlabel('Posición a lo largo de la cuerda (m)')
+ax.set_ylabel('Tiempo (s)')
+ax.set_zlabel('Desplazamiento de la cuerda (m)')
+plt.show()
