77e5654818c3f05c22d4f18f6c5d95e29cd9ac53
jzazooro <91785177+jzazooro@users.noreply.github.com>
2024-03-11 04:54:01 +0100

    fin

--- 2.ecuacion-de-onda/ejercicio4d.py
+++ 2.ecuacion-de-onda/ejercicio4d.py
@@ -9,10 +9,12 @@ b = 6
 d = 24
 n = 600
 m = 2400
-h = b / n
-k = d / m
 v = 0.5 
-p = v * k / h
+
+# Calculando Δx, Δt y r
+Δx = b / n
+Δt = d / m
+r = v * Δt / Δx
 
 # Inicializando la matriz u para almacenar las posiciones de la cuerda
 w = np.zeros((m + 1, n + 1))
@@ -26,17 +28,17 @@ def g(x):
 
 # Condiciones de contorno
 for i in range(1, n):
-    w[0][i] = f(h * i)
-    w[1][i] = w[0][i] + k * g(h * i)
+    w[0][i] = f(Δx * i)
+    w[1][i] = w[0][i] + Δt * g(Δx * i)
 
 for j in range(1, m):
-    w[j][0] = np.sin(k*j)
+    w[j][0] = np.sin(Δt*j)
     w[j][n] = 0
 
 # Aplicando el método de diferencias finitas
 for j in range(1, m):
     for i in range(1, n):
-        w[j+1][i] = 2 * (1 - p*2) * w[j][i] - w[j-1][i] + p*2 * (w[j][i+1] + w[j][i-1])
+        w[j+1][i] = 2 * (1 - r*2) * w[j][i] - w[j-1][i] + r*2 * (w[j][i+1] + w[j][i-1])
 
 # Graficando la cuerda en 3D
 X = np.linspace(0, b, n+1)
