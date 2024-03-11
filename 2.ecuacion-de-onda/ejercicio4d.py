45dad7df24bfd1bfa16dd779203fde1df44c7eba
jzazooro <91785177+jzazooro@users.noreply.github.com>
2024-03-11 04:50:25 +0100

    f

--- 2.ecuacion-de-onda/ejercicio4d.py
+++ 2.ecuacion-de-onda/ejercicio4d.py
@@ -1,21 +1,23 @@
-# d: queremos formar ondas no estacionarias (f=g=0). ¿como lo harias?
-
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
-# Parámetros del dominio y la malla
-b = float(input("Ingrese la longitud de la cuerda (b): "))
-d = float(input("Ingrese el tiempo (d): "))
-N, M = int(input("Número de puntos en la dirección x (N): ")), int(input("Número de puntos en la dirección y (M): "))
-h = b / N
-k = d / M
-v = float(input("Valor de velocidad: "))  
+# ecuacion de onda
+
+# Parámetros dados
+b = 6
+d = 24
+n = 600
+m = 2400
+h = b / n
+k = d / m
+v = 0.5 
 p = v * k / h
 
-# Inicializar la matriz de solución
-w = np.zeros((M + 1, N + 1))
+# Inicializando la matriz u para almacenar las posiciones de la cuerda
+w = np.zeros((m + 1, n + 1))
 
-# Funciones de condiciones iniciales y de contorno
+# Condiciones iniciales
 def f(x):
     return 0
 
@@ -23,35 +25,30 @@ def g(x):
     return 0
 
 # Condiciones de contorno
-for i in range(1, N):
+for i in range(1, n):
     w[0][i] = f(h * i)
     w[1][i] = w[0][i] + k * g(h * i)
 
-for j in range(1, M):
+for j in range(1, m):
     w[j][0] = np.sin(k*j)
-    w[j][N] = 0
+    w[j][n] = 0
 
-# Método de diferencias finitas
-for j in range(1, M):
-    for i in range(1, N):
+# Aplicando el método de diferencias finitas
+for j in range(1, m):
+    for i in range(1, n):
         w[j+1][i] = 2 * (1 - p*2) * w[j][i] - w[j-1][i] + p*2 * (w[j][i+1] + w[j][i-1])
 
-# Crear coordenadas para el gráfico 3D
-X = np.linspace(0, b, N+1)
-Y = np.linspace(0, d, M+1)
+# Graficando la cuerda en 3D
+X = np.linspace(0, b, n+1)
+Y = np.linspace(0, d, m+1)
 X, Y = np.meshgrid(X, Y)
 
-# Crear gráfico 3D
+# Graficando la superficie
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111, projection='3d')
-
-# Graficar la solución en 3D
 ax.plot_surface(X, Y, w, cmap='viridis')
-
-# Configuraciones adicionales del gráfico
 ax.set_xlabel('Posición a lo largo de la cuerda (m)')
 ax.set_ylabel('Tiempo (s)')
 ax.set_zlabel('Desplazamiento de la cuerda (m)')
 
-# Mostrar el gráfico
 plt.show()
